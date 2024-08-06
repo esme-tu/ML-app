4099ba8ee2a5911c50fa0fda70e358c1eb49de02
esme-tu <esramelist@gmail.com>
2024-08-06 15:53:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🧠 ML-App')
-
 st.info('Bu uygulama makine öğrenmesi için yapılan bir uygulamadır 😊')
+
+df=pd.read_cvs('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
