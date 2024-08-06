d78997e9293e21edd6548fe57122ce03aa48c4f2
esme-tu <esramelist@gmail.com>
2024-08-06 18:02:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,6 +75,7 @@ with st.expander('Veri Hazırlama'):
 clf= RandomForestClassifier()
 clf.fit(X,y)
 
+## Tahmin yapmak için modeli uygula
 prediction=clf.predict(input_row)
 prediction_proba=clf.predict_proba(input_row)
 
@@ -84,3 +85,32 @@ df_prediction_proba.rename(columns={0: 'Adelie',
                                  1: 'Chinstrap',
                                  2: 'Gentoo'})
 df_prediction_proba
+# Tahmini türleri görüntüle
+st.subheader('Tahmin Edilen Tür')
+st.dataframe(df_prediction_proba,
+             column_config={
+               'Adelie': st.column_config.ProgressColumn(
+                 'Adelie',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Chinstrap': st.column_config.ProgressColumn(
+                 'Chinstrap',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+               'Gentoo': st.column_config.ProgressColumn(
+                 'Gentoo',
+                 format='%f',
+                 width='medium',
+                 min_value=0,
+                 max_value=1
+               ),
+             }, hide_index=True)
+
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.success(str(penguins_species[prediction][0]))
